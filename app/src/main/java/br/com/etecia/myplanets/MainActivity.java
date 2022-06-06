0ba1e52450e0da2d2bd8ab3cc14cc626f80ce78a
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-06-06 20:19:07 -0300

    Lista Funcionando

--- app/src/main/java/br/com/etecia/myplanets/MainActivity.java
+++ app/src/main/java/br/com/etecia/myplanets/MainActivity.java
@@ -1,26 +1,28 @@
 package br.com.etecia.myplanets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     //Declarar lista
     ListView lista_planetas;
 
     //criando vetores com as imagens e nomes
 
-    String nome_planetas [] = {"Jupiter", "Lua", "Marte", "Mercurio", "Netuno", "Plutão", "Saturno", "Sol", "Terra", "Urano", "Venus"};
+    String nome_planetas[] = {"Jupiter", "Lua", "Marte", "Mercurio", "Netuno", "Plutão", "Saturno", "Sol", "Terra", "Urano", "Venus"};
+    int imagem_planetas[] = {R.drawable.jupiter, R.drawable.lua, R.drawable.marte, R.drawable.mercurio, R.drawable.netuno, R.drawable.plutao, R.drawable.saturno, R.drawable.sol, R.drawable.terra, R.drawable.urano, R.drawable.venus};
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lista_planetas = findViewById(R.id.lista_planetas);
@@ -30,12 +32,11 @@ public class MainActivity extends AppCompatActivity
     }
 
     //criando o adaptador como classe interna
-    public class Adapter extends BaseAdapter
-    {
+    public class Adapter extends BaseAdapter {
 
         @Override
         public int getCount() {
-            return 0;
+            return imagem_planetas.length;
         }
 
         @Override
@@ -50,7 +51,20 @@ public class MainActivity extends AppCompatActivity
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+           //itens que vão no modelo
+            ImageView img_planeta;
+            TextView txt_planeta;
+            CardView card_planeta;
+            View modelo = getLayoutInflater().inflate(R.layout.modelo_planetas, null);
+
+            txt_planeta = modelo.findViewById(R.id.txt_modelo_planeta);
+            img_planeta = modelo.findViewById(R.id.img_modelo_planeta);
+
+            //preenchendo o modelo com valores do vetor
+            txt_planeta.setText(nome_planetas[i]);
+            img_planeta.setImageResource(imagem_planetas[i]);
+
+            return modelo;
         }
     }
 }
